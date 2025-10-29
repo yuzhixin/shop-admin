4f83f0a2f8ebc8765f531c7e5c231e4d75f70368
水水 <kdgzs2010@163.com>
2025-10-29 17:16:59 +0800

    debug

--- shop/admin.py
+++ shop/admin.py
@@ -6,34 +6,28 @@ from .models import WechatUser, Tag, Goods, Order
 class WechatUserAdmin(admin.ModelAdmin):
     """微信用户管理界面配置"""
     list_display = (
-        'username', 'nickname', 'openid', 'email', 'is_active', 'date_joined'
+        'nickname', 'openid', 'avatar_url', 'created_at', 'updated_at'
     )
     list_filter = (
-        'is_active', 'is_staff', 'is_superuser', 'date_joined'
+        'created_at',
     )
     search_fields = (
-        'username', 'nickname', 'openid', 'email', 'first_name', 'last_name'
+        'nickname', 'openid'
     )
-    ordering = ('-date_joined',)
-    readonly_fields = ('openid', 'session_key', 'date_joined', 'last_login')
+    ordering = ('-created_at',)
+    readonly_fields = ('openid', 'session_key', 'created_at', 'updated_at')
 
     fieldsets = (
         ('基本信息', {
             'fields': (
-                'username', 'nickname', 'email', 'first_name', 'last_name'
+                'nickname', 'avatar_url'
             )
         }),
         ('微信信息', {
-            'fields': ('openid', 'avatar_url', 'session_key')
-        }),
-        ('权限状态', {
-            'fields': (
-                'is_active', 'is_staff', 'is_superuser',
-                'groups', 'user_permissions'
-            )
+            'fields': ('openid', 'session_key')
         }),
         ('时间信息', {
-            'fields': ('date_joined', 'last_login')
+            'fields': ('created_at', 'updated_at')
         }),
     )
 
